59ecb9b3378ff042a260dcd013ce3c6e04827537
Derick Ostrenko <dostrenko@gmail.com>
2014-07-15 12:26:40 -0500

    sockets working

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var logger = require('morgan');
 var app = express();
+var server = app.listen(3000);
 
 // log requests
 app.use(logger('dev'));
@@ -12,20 +13,37 @@ app.use(logger('dev'));
 // serve content from /public
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000);
 console.log('Listening on port %d', server.address().port);
+
+/////////////
+//socket.io//
+/////////////
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+
+    socket.on('message', function(data) {
+    	console.log(data);
+    });
+
+    socket.on('disconnect', function() {
+    	console.log("bye");
+    });
+
+});
 
 ///////////////
 //node-canvas//
 ///////////////
 
-var Canvas = require('canvas')
-  , Image = Canvas.Image
-  , canvas = new Canvas(200,200)
-  , ctx = canvas.getContext('2d');
+var Canvas = require('canvas'),
+    Image = Canvas.Image,
+    canvas = new Canvas(200, 200),
+    ctx = canvas.getContext('2d');
 
 ctx.font = '30px Impact';
-ctx.rotate(.1);
+ctx.rotate(0.1);
 ctx.fillText("Awesome!", 50, 100);
 
 var te = ctx.measureText('Awesome!');
@@ -35,4 +53,4 @@ ctx.lineTo(50, 102);
 ctx.lineTo(50 + te.width, 102);
 ctx.stroke();
 
-console.log('<img src="' + canvas.toDataURL() + '" />');
+// console.log('<img src="' + canvas.toDataURL() + '" />');
