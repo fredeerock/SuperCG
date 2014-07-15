8a2fbd2d461bbf7e58cee2a992ed88709bcbb2b1
Derick Ostrenko <dostrenko@gmail.com>
2014-07-15 11:34:12 -0500

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/**
- * Module dependencies.
- */
+///////////
+//Express//
+///////////
 
 var express = require('express');
 var logger = require('morgan');
@@ -9,32 +9,30 @@ var app = express();
 // log requests
 app.use(logger('dev'));
 
-// express on its own has no notion
-// of a "file". The express.static()
-// middleware checks for a file matching
-// the `req.path` within the directory
-// that you pass it. In this case "GET /js/app.js"
-// will look for "./public/js/app.js".
-
+// serve content from /public
 app.use(express.static(__dirname + '/public'));
 
-// if you wanted to "prefix" you may use
-// the mounting feature of Connect, for example
-// "GET /static/js/app.js" instead of "GET /js/app.js".
-// The mount-path "/static" is simply removed before
-// passing control to the express.static() middleware,
-// thus it serves the file correctly by ignoring "/static"
-// app.use('/static', express.static(__dirname + '/public'));
+var server = app.listen(3000);
+console.log('Listening on port %d', server.address().port);
 
-// if for some reason you want to serve files from
-// several directories, you can use express.static()
-// multiple times! Here we're passing "./public/css",
-// this will allow "GET /style.css" instead of "GET /css/style.css":
-// app.use(express.static(__dirname + '/public/css'));
+///////////////
+//node-canvas//
+///////////////
 
-app.listen(3000);
-console.log('listening on port 3000');
-console.log('try:');
-console.log('  GET /hello.txt');
-console.log('  GET /js/app.js');
-console.log('  GET /css/style.css');
+var Canvas = require('canvas')
+  , Image = Canvas.Image
+  , canvas = new Canvas(200,200)
+  , ctx = canvas.getContext('2d');
+
+ctx.font = '30px Impact';
+ctx.rotate(.1);
+ctx.fillText("Awesome!", 50, 100);
+
+var te = ctx.measureText('Awesome!');
+ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.lineTo(50 + te.width, 102);
+ctx.stroke();
+
+console.log('<img src="' + canvas.toDataURL() + '" />');
